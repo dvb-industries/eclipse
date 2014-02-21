c1ff28d7f88a280be99b4085bff2aebb06051e75
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-21 12:51:17 +0100

    correct recipe after test
    fixes #1

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,25 +1,39 @@
 package "wget"
 
 execute "create bin" do
-  command "mkdir -p #{node[:eclips][:bin]}"
+  command "mkdir -p #{node[:eclipse][:bin]}"
   user node[:eclipse][:user]
-  group node [:eclipse][:group]
-  creates "#{node[:eclipse][:bin]}"
+  group node[:eclipse][:group]
+  creates node[:eclipse][:bin]
 end
 
 execute "wget eclipse" do
   cwd node[:eclipse][:download]
-  command "wget #{node[:eclipse][:url]}#{node[:eclipse][:package]}"
+  command "wget -O #{node[:eclipse][:package]} #{node[:eclipse][:url]}#{node[:eclipse][:package]}"
+  user node[:eclipse][:user]
+  group node[:eclipse][:user]
   creates "#{node[:eclipse][:download]}${node[:eclipse][:package]}"
 end
 
 execute "tar xvfz eclipse" do
   cwd node[:eclipse][:bin]
   command "tar xvfz #{node[:eclipse][:download]}#{node[:eclipse][:package]}"
+  user node[:eclipse][:user]
+  group node[:eclipse][:user]
+  creates "#{node[:eclipse][:bin]}eclipse"
 end
 
-execute "ln -s eclipse" do
+execute "mv eclipse" do
   cwd node[:eclipse][:bin]
-  # should actually be a link
-  command "ln -s eclipse #{node[:eclipse][:package_name]}"
+  command "mv eclipse #{node[:eclipse][:package_name]}"
+  user node[:eclipse][:user]
+  group node[:eclipse][:user]
+  creates "#{node[:eclipse][:bin]}${node[:eclipse][:package_name]}"
+end
+
+link "ln -s eclipse" do
+  target_file "#{node[:eclipse][:bin]}eclipse"
+  to "#{node[:eclipse][:bin]}#{node[:eclipse][:package_name]}/eclipse"
+  user node[:eclipse][:user]
+  group node[:eclipse][:user]
 end
